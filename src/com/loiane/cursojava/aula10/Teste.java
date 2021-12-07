2654e125a272bfa5b3525a9b2bb8e49bbe1f0b1c
misael <mbizeli@gmail.com>
2021-12-07 16:20:19 -0300

    curso de java da Loiane

---
+++ src/com/loiane/cursojava/aula10/Teste.java
@@ -0,0 +1,11 @@
+package com.loiane.cursojava.aula10;
+
+public class Teste {
+    public static void main(String[] args) {
+        int age = 20;
+        String name = "Misael";
+        System.out.println("meu nome é:" + name);
+        System.out.println("idade:" + age);
+    }
+        
+}
---
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+    public static void main(String[] args){
+
+        int age;
+        String nome =  "Misael";
+        String nomeDoMeuCachorro = "Bela";
+
+        age = 25;
+        System.out.println("Idade = " + age);
+        System.out.println("Nome = " + nome);
+    }
+}
+
